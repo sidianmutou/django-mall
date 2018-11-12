eb05fc5e133df4ad0d6561cc85e1449eef8fea0b
olex1216 <olex1216@gmail.com>
2018-11-12 23:36:53 +0800

    商品详情也，用户收藏

---
+++ apps/user_operation/serializers.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/12 23:07
+# @Author  : Olex
+# @Email   : olex1216@gmail.com
+# @File    : serializers.py
+# @Software: PyCharm
---
+++ apps/users/serializers.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/11 22:44
+# @Author  : Olex
+# @Email   : olex1216@gmail.com
+# @File    : serializers.py
+# @Software: PyCharm
